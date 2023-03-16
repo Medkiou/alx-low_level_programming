ed590b45c486febae33db503169e1a39212c6d47
Medkiou <meddakh123@gmail.com>
2023-03-16 05:55:46 -0700

    16/04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
- * main - print Programming is like building a multilingual puzzle whithe puts
+ * main - Entry point
  *
- * Return:0 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
