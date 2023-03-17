073835575bdec2a27f275c9384a2b8c9986b0c8c
Medkiou <meddakh123@gmail.com>
2023-03-17 11:42:17 -0700

    17/03

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,27 +4,26 @@
  * Return: 0 (Success)
  */
 int main(void)
+{ 
+int a, b, c;
+for (a = 48; a <= 55; a++)
 {
-        int a, b, c;
-
-        for (a = 48; a <= 55; a++)
-        {
-                for (b = 49; b <= 56; b++)
-                {
-			for (c = 50; c <= 57; c++)
-                        if (a <  b || b < c)
-                        {
-                                putchar(a);
-                                putchar(b);
-				putchar(c);
-                                if (a != 55 || b != 56 || c!=57)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+for (b = 49; b <= 56; b++)
+{
+for (c = 50; c <= 57; c++)
+if (a <  b && b < c)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a != 55 || b != 56 || c!=57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
