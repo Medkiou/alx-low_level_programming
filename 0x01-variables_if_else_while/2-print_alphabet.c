5014c2b47e308899cb5af90f782b284655cfdb28
Medkiou <meddakh123@gmail.com>
2023-03-17 04:55:46 -0700

    17/03

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+while (i < 26)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i = 0;
-
-	while (i < 26)
-	{
-		putchar(alp[i]);
-           	i++;
-       	}
-        putchar('\n');	
-	return (0);
+putchar(alp[i]);
+i++; }
+putchar('\n');
+return (0);
 }
