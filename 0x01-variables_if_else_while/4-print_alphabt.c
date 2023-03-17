e07cdcb6df2c4bd0792b0a4b476ad44219724d46
Medkiou <meddakh123@gmail.com>
2023-03-17 04:37:02 -0700

    17/03

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,15 +5,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-        char alp[24] = "abcdfghijklmnoprstuvwxyz";
-        int i = 0;
-
-        while (i < 24)
-        {
-                putchar(alp[i]);
-                i++;
-        }
-        putchar('\n');
-        return (0);
+{char alp[24] = "abcdfghijklmnoprstuvwxyz";
+ int i = 0;
+while (i < 24) {  putchar(alp[i]); i++; }
+ putchar('\n');
+return (0);
 }
