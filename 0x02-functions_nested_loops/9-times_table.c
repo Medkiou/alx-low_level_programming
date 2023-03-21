115fa3d6d856a3694f752729e043e06cec65b16c
Medkiou <meddakh123@gmail.com>
2023-03-21 15:48:15 -0700

    21/03

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,25 @@ void times_table(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
-		
-			if (j == 0)
+		{
+			if ((i * j) < 10)
 			{
-				_putchar((j * i) + '0');
-			}
-
-			if ((j * i) < 10 && j != 0)
+				_putchar((i * j) + '0');
+				if (j != 9)
+				{
+					_putchar('  ');
+					_putchar(' ');
+				}
+			else
 			{
-				_putchar(',');
-				_putchar('  ');
-				_putchar((j * i) + '0');
-			} else if ((j * i) >= 10)
-			{
-				_putchar(',');
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
 				_putchar(' ');
-				_putchar(((j * i) / 10) + '0');
-				_putchar(((j * i) % 10) + '0');
+				_putchar(',');
+			}
+			_putchar('\n');
 			}
 		}
-		_putchar('\n');
 	}
 }
+			
