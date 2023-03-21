2e49ebba26352584610f705de8b617eff9538464
Medkiou <meddakh123@gmail.com>
2023-03-21 15:22:44 -0700

    21/03

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,12 +6,12 @@
 void times_table(void)
 {
 	int i, j;
-	for (i = 48; i <= 57 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (j = 48; j <= 57 ; j++)
+		for (j = 0 ; j <= 9 ; j++)
 		{
-			_putchar(j * i);
-			if (j != 57)
+			_putchar((j * i) + '0');
+			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
