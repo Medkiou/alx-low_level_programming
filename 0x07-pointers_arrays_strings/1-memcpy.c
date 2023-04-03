ddc1cf69a9f2179f5abcd60ae22ffdf49d17178a
Medkiou <meddakh123@gmail.com>
2023-04-03 11:04:21 -0700

    03/04

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int r;
-	for (r = 0; r < n; r++)
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
 	{
 		dest[r] = src[r];
 		n--;
 	}
 	return (dest);
 }
+
