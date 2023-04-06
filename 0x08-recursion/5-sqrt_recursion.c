d09609b3299e0f34fd2c307afed45c082a82fb20
Medkiou <meddakh123@gmail.com>
2023-04-06 15:01:27 -0700

    05/04

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,42 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
+ * _sqrt_helper - helper function for calculating natural square root
+ * @n: the number to find the natural square root of
+ * @low: the lower bound of the binary search range
+ * @high: the upper bound of the binary search range
  *
- * Return: the resulting square root
+ * Return: the natural square root of n if it exists, otherwise -1
+ */
+int _sqrt_helper(int n, int low, int high)
+{
+    int mid, mid_squared;
+
+    /* Base case: n has a natural square root */
+    if (low > high)
+        return (-1);
+
+    mid = (low + high) / 2;
+    mid_squared = mid * mid;
+
+    if (mid_squared == n)
+        return (mid);
+    else if (mid_squared < n)
+        return (_sqrt_helper(n, mid + 1, high));
+    else
+        return (_sqrt_helper(n, low, mid - 1));
+}
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number
+ * @n: the number to find the natural square root of
+ *
+ * Return: the natural square root of n if it exists, otherwise -1
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+    if (n < 0)
+        return (-1);
+
+    return (_sqrt_helper(n, 0, n));
 }
